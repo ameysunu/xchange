2102caa5f5487205c496dbba6bd6455a51306b2a
ameysunu <ameysunu00719@gmail.com>
2021-12-27 21:15:34 +0000

    Initial Commit

---
+++ xChange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xChange
+//
+//  Created by Amey Sunu on 27/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xChange/xChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xChangeApp.swift
+//  xChange
+//
+//  Created by Amey Sunu on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct xChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
