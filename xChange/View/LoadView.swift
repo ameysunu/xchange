51b0f5b6d09189b63a89f1a9314c79b1d1ad9cda
ameysunu <ameysunu00719@gmail.com>
2021-12-30 12:50:27 +0000

    added home screen and navigation

---
+++ xChange/View/LoadView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadView.swift
+//  xChange
+//
+//  Created by Amey Sunu on 30/12/21.
+//
+
+import SwiftUI
+
+struct LoadView: View {
+    var body: some View {
+        ZStack {
+            Text("Getting Started with xChange")
+        }
+    }
+}
+
+struct LoadView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadView()
+    }
+}
