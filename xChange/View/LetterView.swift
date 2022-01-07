e9d4e16947e171ccb396464b824c05e68044d715
ameysunu <ameysunu00719@gmail.com>
2022-01-07 21:13:52 +0000

    maps added

--- xChange/View/LetterView.swift
+++ xChange/View/LetterView.swift
@@ -6,10 +6,31 @@
 //
 
 import SwiftUI
+import CoreLocation
+import MapKit
 
 struct LetterView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
     var body: some View {
-        Text("LetterView")
+        VStack {
+            Text("location status: \(locationManager.statusString)")
+            Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: CLLocationDegrees(userLatitude) ?? 0, longitude: CLLocationDegrees(userLongitude) ?? 0), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))), interactionModes: [])
+                .frame(width: 400, height: 300)
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+            }
+        }
     }
 }
 
